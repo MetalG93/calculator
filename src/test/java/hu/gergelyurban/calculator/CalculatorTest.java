31a4aed0b10fc2709f77f05c9063d6e8b114b6d7
Gergely Urbán <gergely.urban@frontside.hu>
2021-11-26 11:14:40 +0100

    Add sum logic, controller and unit test

---
+++ src/test/java/hu/gergelyurban/calculator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package hu.gergelyurban.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        Assertions.assertEquals(5, calculator.sum(2, 3));
+    }
+}
